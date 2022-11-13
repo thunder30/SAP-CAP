6e9a7f7bca959dc2bbfcee4122fa60585ee31b7c
thunder30 <ngoctinh3004@gmail.com>
2022-11-13 12:21:04 +0700

    Create app SAP-CAP

---
+++ app/services.cds
@@ -0,0 +1,2 @@
+
+using from './test_fiori_app/annotations';
---
+++ app/test_fiori_app/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,26 @@
+sap.ui.require(
+    [
+        'sap/fe/test/JourneyRunner',
+        'testfioriapp/test/integration/FirstJourney',
+		'testfioriapp/test/integration/pages/RisksList',
+		'testfioriapp/test/integration/pages/RisksObjectPage'
+    ],
+    function(JourneyRunner, opaJourney, RisksList, RisksObjectPage) {
+        'use strict';
+        var JourneyRunner = new JourneyRunner({
+            // start index.html in web folder
+            launchUrl: sap.ui.require.toUrl('testfioriapp') + '/index.html'
+        });
+
+       
+        JourneyRunner.run(
+            {
+                pages: { 
+					onTheRisksList: RisksList,
+					onTheRisksObjectPage: RisksObjectPage
+                }
+            },
+            opaJourney.run
+        );
+    }
+);
